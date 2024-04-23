2756eb3eb0916353555e6b8a3939dc54d0439aa9
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-04-23 18:04:31 +0600

    preview modal section done

--- src/Components/Shop/DealSection.jsx
+++ src/Components/Shop/DealSection.jsx
@@ -6,95 +6,121 @@ import Rating from "react-rating-stars-component";
 import { BsCart3 } from "react-icons/bs";
 import { LuView } from "react-icons/lu";
 import { MdCompare } from "react-icons/md";
+import ViewProductModal from "./ViewProductModal";
+
 const DealSection = () => {
   const [showAll, setShowAll] = useState(false);
-  console.log({ showAll });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalData, setModalData] = useState([]);
+  console.log(modalData);
 
+  const passData = (id) => {
+    const findProduct = data?.find((p) => p.id === id);
+    setModalData(findProduct);
+  };
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
   return (
-    <div className="mt-16">
-      <div className="flex justify-between items-center mb-6">
-        <h4 className="text-2xl font-semibold ">Deal of The Day</h4>
-        <div>
-          {!showAll ? (
-            <button
-              onClick={() => setShowAll(true)}
-              className="font-bold hover:text-blue-950"
-            >
-              See More Items &gt;{" "}
-            </button>
-          ) : (
-            <button
-              onClick={() => setShowAll(false)}
-              className="font-bold hover:text-blue-950"
-            >
-              &lt; See Less Items
-            </button>
-          )}
-        </div>
-      </div>
-      <div className="grid grid-cols-2 gap-6 ">
-        {/* card-section */}
-        {data?.slice(0, showAll ? data?.length : 2).map((item, index) => (
-          <div
-            key={index}
-            className="flex gap-3 justify-between items-start bg-white p-4"
-          >
-            <div className="w-[49%] h-full">
-              <img src={item?.img} alt="img" className="w-100 h-[100%]" />
-            </div>
-            {/* card-description */}
-            <div className="w-[49%] space-y-2">
-              <h5 className="font-bold text-xl">{item?.name}</h5>
-              <div className="flex items-center gap-2">
-                <Rating
-                  count={5}
-                  size={18}
-                  value={item?.rating || 0}
-                  edit={false}
-                  activeColor="#ffd700"
-                  emptyIcon={<FaStar />}
-                  filledIcon={<FaStar />}
-                />
-                <span>{item?.rating}</span>
-                <span>{`(${item?.review || 0} Review)`}</span>
-              </div>
-              <div className="flex justify-start items-center gap-5">
-                <span className="text-2xl font-bold">${item?.price}</span>
-                <del className="text-gray-500">${item?.previousPrice}</del>
-                <span className="text-gray-500">
-                  {`-${(
-                    ((item?.previousPrice - item?.price) /
-                      item?.previousPrice) *
-                    100
-                  ).toFixed(2)}%`}
-                </span>
-              </div>
-              <p className="text-gray-600">{item?.description}</p>
-              <div className="flex justify-around items-center bg-[#f0f0f0] p-2">
-                <button className="flex justify-center gap-1 items-center hover:text-blue-800 ">
-                  <LuView></LuView> Preview
-                </button>
-                <button className="flex justify-center gap-1 items-center hover:text-blue-800">
-                  <MdCompare /> Compare
-                </button>
-              </div>
-              <button className="btn-primary w-full flex justify-center gap-2 items-center">
-                <BsCart3 className="text-lg "></BsCart3> Add to Cart
+    <>
+      <div className="mt-16">
+        <div className="flex justify-between items-center mb-6">
+          <h4 className="text-2xl font-semibold ">Deal of The Day</h4>
+          <div>
+            {!showAll ? (
+              <button
+                onClick={() => setShowAll(true)}
+                className="font-bold hover:text-blue-950"
+              >
+                See More Items &gt;{" "}
               </button>
-            </div>
+            ) : (
+              <button
+                onClick={() => setShowAll(false)}
+                className="font-bold hover:text-blue-950"
+              >
+                &lt; See Less Items
+              </button>
+            )}
           </div>
-        ))}
-      </div>
-      {/* another part */}
-      <div className="flex w-100 justify-between mt-6">
-        <div className="border w-[49%]">
-          <img src={img} alt="sofa Aqua" />
         </div>
-        <div className="border w-[49%]">
-          <img src={img} alt="sofa Aqua" />
+        <div className="grid grid-cols-2 gap-6 ">
+          {/* card-section */}
+          {data?.slice(0, showAll ? data?.length : 2).map((item, index) => (
+            <div
+              key={index}
+              className="flex gap-3 justify-between items-start bg-white p-4"
+            >
+              <div className="w-[49%] h-full">
+                <img src={item?.img} alt="img" className="w-100 h-[100%]" />
+              </div>
+              {/* card-description */}
+              <div className="w-[49%] space-y-2">
+                <h5 className="font-bold text-xl">{item?.name}</h5>
+                <div className="flex items-center gap-2">
+                  <Rating
+                    count={5}
+                    size={18}
+                    value={item?.rating || 0}
+                    edit={false}
+                    activeColor="#ffd700"
+                    emptyIcon={<FaStar />}
+                    filledIcon={<FaStar />}
+                  />
+                  <span>{item?.rating}</span>
+                  <span>{`(${item?.review || 0} Review)`}</span>
+                </div>
+                <div className="flex justify-start items-center gap-5">
+                  <span className="text-2xl font-bold">${item?.price}</span>
+                  <del className="text-gray-500">${item?.previousPrice}</del>
+                  <span className="text-gray-500">
+                    {`-${(
+                      ((item?.previousPrice - item?.price) /
+                        item?.previousPrice) *
+                      100
+                    ).toFixed(2)}%`}
+                  </span>
+                </div>
+                <p className="text-gray-600">{item?.description}</p>
+                <div className="flex justify-around items-center bg-[#f0f0f0] p-2">
+                  <button
+                    title="Click here to show modal Preview"
+                    onClick={() => {
+                      passData(item?.id);
+                      openModal();
+                    }}
+                    className="flex justify-center gap-1 items-center hover:text-blue-800 "
+                  >
+                    <LuView></LuView> Preview
+                  </button>
+                  <button className="flex justify-center gap-1 items-center hover:text-blue-800">
+                    <MdCompare /> Compare
+                  </button>
+                </div>
+                <button className="btn-primary w-full flex justify-center gap-2 items-center">
+                  <BsCart3 className="text-lg "></BsCart3> Add to Cart
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        {/* another part */}
+        <div className="flex w-100 justify-between mt-6">
+          <div className="border w-[49%]">
+            <img src={img} alt="sofa Aqua" />
+          </div>
+          <div className="border w-[49%]">
+            <img src={img} alt="sofa Aqua" />
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* modal section */}
+      <ViewProductModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        item={modalData}
+      ></ViewProductModal>
+    </>
   );
 };
 
---
+++ src/Components/Shop/ViewProductModal.jsx
@@ -0,0 +1,84 @@
+import React, { useRef, useEffect } from "react";
+import { FaStar } from "react-icons/fa";
+import Rating from "react-rating-stars-component";
+
+const ViewProductModal = ({ isOpen, onClose, item }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleOutsideClick);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <div className={`fixed inset-0 z-50 ${isOpen ? "block" : "hidden"}`}>
+      <div className="modal-overlay" onClick={onClose}></div>
+      <div className="">
+        <div
+          ref={modalRef}
+          className="modal-content bg-white rounded-lg shadow-md p-4 mx-auto mt-24 w-1/2"
+        >
+          <div className="flex gap-3 justify-between items-start bg-white p-4">
+            <div className="w-[49%] h-full">
+              <img src={item?.img} alt="img" className="w-100 h-[100%]" />
+            </div>
+            <div className="w-[49%] space-y-3 flex flex-col justify-between h-[41vh] ">
+              <div>
+                <h5 className="font-bold text-xl">{item?.name}</h5>
+                <div className="flex items-center gap-2">
+                  <Rating
+                    count={5}
+                    size={18}
+                    value={item?.rating || 0}
+                    edit={false}
+                    activeColor="#ffd700"
+                    emptyIcon={<FaStar />}
+                    filledIcon={<FaStar />}
+                  />
+                  <span>{item?.rating}</span>
+                  <span>{`(${item?.review || 0} Review)`}</span>
+                </div>
+                <div className="flex justify-start items-center gap-5">
+                  <span className="text-2xl font-bold">${item?.price}</span>
+                  <del className="text-gray-500">${item?.previousPrice}</del>
+                  <span className="text-gray-500">
+                    {`-${(
+                      ((item?.previousPrice - item?.price) /
+                        item?.previousPrice) *
+                      100
+                    ).toFixed(2)}%`}
+                  </span>
+                </div>
+                <p className="text-gray-600">
+                  {item?.description} {item?.description}
+                </p>
+              </div>
+
+              <div className="mt-auto">
+                <button
+                  onClick={onClose}
+                  className="btn-danger w-full flex justify-center gap-2 items-center"
+                >
+                  Close Modal
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewProductModal;
